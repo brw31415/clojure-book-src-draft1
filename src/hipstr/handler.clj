74d8759cb7eae2a2cd2ebd3c7d7cc6dc02107774
ryanbaldwin <ryanbaldwin@gmail.com>
2014-09-03 21:02:09 -0400

    started working on the selmer section of the chapter

--- src/hipstr/handler.clj
+++ src/hipstr/handler.clj
@@ -1,6 +1,7 @@
 (ns hipstr.handler
   (:require [compojure.core :refer [defroutes]]
             [hipstr.routes.home :refer [home-routes]]
+            [hipstr.routes.test-routes :refer [test-routes]]
             [hipstr.middleware :refer [load-middleware]]
             [hipstr.session-manager :as session-manager]
             [noir.response :refer [redirect]]
@@ -47,7 +48,7 @@
 
 (def app (app-handler
            ;; add your application routes here
-           [home-routes base-routes]
+           [home-routes base-routes test-routes]
            ;; add custom middleware here
            :middleware (load-middleware)
            ;; timeout sessions after 30 minutes
---
+++ src/hipstr/routes/test_routes.clj
@@ -0,0 +1,20 @@
+(ns hipstr.routes.test-routes
+  (:require [compojure.core :refer :all]))
+
+(defn render-request-val [m & [k]]
+  "Simply returns the value of key k in map m, if k is provided;
+   Otherwise return the original m.
+   If k is provided, but not found in m, a message indicating as such
+   will be returned. In real life you'll probably want to just return nil,
+   or raise an exception... doing what I'm doing here will get you raged on."
+  (let [result (if k
+                (or ((keyword k) m) (str k "is not a valid key."))
+                m)]
+    (str result)))
+
+(defroutes test-routes
+  (GET "/req" request (render-request request))
+  ;(GET "/req/:val" [val] (str val)) ;no access to the full request map
+  (GET "/req/:val" [val :as full-req] (str val "<br>" full-req)) ; use :as to get access to full request map
+  (GET "/req/key/:key" [key :as request] (render-request-val request key))
+  (GET "/req/:val/:another-val/:and-another" [val & remainders] (str val "<br>" remainders))) ;use & to get access to unbound params
