8f74fdae9169aa106fb0f87aea248637334a8c08
ryanbaldwin <ryanbaldwin@gmail.com>
2014-09-06 12:13:09 -0400

    adjusted for readability.

--- src/hipstr/routes/test_routes.clj
+++ src/hipstr/routes/test_routes.clj
@@ -7,14 +7,24 @@
    If k is provided, but not found in m, a message indicating as such
    will be returned. In real life you'll probably want to just return nil,
    or raise an exception... doing what I'm doing here will get you raged on."
-  (let [result (if k
-                (or ((keyword k) m) (str k "is not a valid key."))
-                m)]
-    (str result)))
+  (str (if k
+         (if-let [result ((keyword k) m)]
+           result
+           (str k " is not a valid key."))
+         m)))
 
 (defroutes test-routes
   (GET "/req" request (render-request-val request))
-  ;(GET "/req/:val" [val] (str val)) ;no access to the full request map
-  (GET "/req/:val" [val :as full-req] (str val "<br>" full-req)) ; use :as to get access to full request map
-  (GET "/req/key/:key" [key :as request] (render-request-val request key))
-  (GET "/req/:val/:another-val/:and-another" [val & remainders] (str val "<br>" remainders))) ;use & to get access to unbound params
+
+  ;no access to the full request map
+  ;(GET "/req/:val" [val] (str val))
+
+  ; use :as to get access to full request map
+  (GET "/req/:val" [val :as full-req] (str val "<br>" full-req))
+
+  ;use & to get access to unbound params
+  (GET "/req/:val/:another-val/:and-another" [val & remainders] (str val "<br>" remainders))
+
+    ; order is important. /req/:key will never match because
+  ; /req/:val precedes it. Routes are matched on URL structure.
+  (GET "/req/key/:key" [key :as request] (render-request-val request key)))
